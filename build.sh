e81ff399fc0dc27bcfcaad3bf9b376f4a06fa0ff
Rabo Serg <raboserg@gmail.com>
2019-12-05 17:49:11 +0300

    system build refactoring

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-rem setx /m OVPN3_HOME_DIR "D:/library/openvpn3"
-rmdir /S /Q build
+export OVPN3_HOME_DIR "D:/library/openvpn3"
+rm build
 mkdir build
 cd build
 cmake -DCMAKE_TOOLCHAIN_FILE=D:\library\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_GENERATOR_PLATFORM=x64 ..
